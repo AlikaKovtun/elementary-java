9863fcccf9f738d062f2528507ebd63b5bfca1d0
AlikaKovtun <kovtun.alika@gmail.com>
2021-03-14 19:41:25 +0200

    [HW-05] version1

--- src/main/java/ArrayStorage.java
+++ src/main/java/ArrayStorage.java
@@ -4,7 +4,8 @@ package main.java;
  * Array based storage for Employees
  */
 public class ArrayStorage {
-    Employee[] storage = new Employee[10000];
+    private Employee[] storage = new Employee[10000];
+    private int empCounter = 0;
 
     void clear() {
         //Получаем количество сохраненных работников и обнуляем их в массиве
@@ -12,13 +13,14 @@ public class ArrayStorage {
         for (int i = 0; i < numberOfEmp; i++) {
             storage[i] = null;
         }
+        empCounter = 0;
         System.out.println("The list is clear!");
     }
 
     void save(Employee employee) {
         //Проверяем нет ли такого работника уже в списке
         for (Employee e : storage) {
-            if (e != null && e.uuid.equals(employee.uuid)) {
+            if (e != null && e.getUuid().equals(employee.getUuid())) {
                 System.out.println("Employee " + employee + " is already saved");
                 return;
             }
@@ -32,17 +34,15 @@ public class ArrayStorage {
         //Сохраняем сотрудника в первой пустой ячейке
         //Выводим сообщение об удачном сохранении и выходим из метода
         storage[numberOfEmp] = employee;
+        empCounter++;
         System.out.println("Employee " + employee + " saved!");
     }
 
     Employee get(String uuid) {
-        int numberOfEmp = size();
+        int index = equalsEmpUuid(uuid);
         Employee temp = null;
-        for (int i = 0; i < numberOfEmp; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                temp = storage[i];
-                break;
-            }
+        if (index != -1) {
+            temp = storage[index];
         }
         if (temp == null) {
             System.out.println("The list doesn't contain an employee with such uuid");
@@ -55,14 +55,14 @@ public class ArrayStorage {
     //Находим нужного работника на удаление и помещаем в эту ячейку последнего работника в списке
     //Если работника с таким uuid нет, выводим сообщение об ошибке
     void delete(String uuid) {
+        int index = equalsEmpUuid(uuid);
         int numberOfEmp = size();
-        for (int i = 0; i < numberOfEmp; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                storage[i] = storage[numberOfEmp - 1];
-                storage[numberOfEmp - 1] = null;
-                System.out.println("Employee " + uuid + " deleted");
-                return;
-            }
+        if (index != -1) {
+            storage[index] = storage[numberOfEmp - 1];
+            storage[numberOfEmp - 1] = null;
+            System.out.println("Employee " + uuid + " deleted");
+            empCounter--;
+            return;
         }
         System.out.println("The list doesn't contain an employee with such uuid");
     }
@@ -82,11 +82,15 @@ public class ArrayStorage {
     }
 
     int size() {
-        int numberOfEmp = 0;
-        for (Employee e : storage) {
-            if (e == null) break;
-            numberOfEmp++;
+        return empCounter;
+    }
+
+    private int equalsEmpUuid(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return i;
+            }
         }
-        return numberOfEmp;
+        return -1;
     }
 }
--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -6,7 +6,20 @@ package main.java;
 public class Employee {
 
     // Unique identifier
-    String uuid;
+   private String uuid;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        if(uuid.matches("uuid[1-9]\\d*")){
+            this.uuid = uuid;
+        }
+       else {
+            System.out.println("uuid is not correct!");
+        }
+    }
 
     @Override
     public String toString() {
